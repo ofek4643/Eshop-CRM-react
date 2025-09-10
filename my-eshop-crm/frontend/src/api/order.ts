80415a0760847a8af26119fdd74f041d3c452c51
ofek4643 <ofekddia1@gmail.com>
2025-09-10 14:57:34 +0300

    getOrder

--- my-eshop-crm/frontend/src/api/order.ts
+++ my-eshop-crm/frontend/src/api/order.ts
@@ -7,7 +7,7 @@ export const getOrdersApi = () => api.get("/order/admin");
 export const deleteOrderApi = (id: string) => api.delete(`/order/admin/${id}`);
 
 // שליפת הזמנה לפי ID
-export const getOrderApi = (id: string) => api.get(`/order/${id}`);
+export const getOrderApi = (id: string) => api.get(`/order/admin/${id}`);
 
 // שליפת הזמנות לפי USER
 export const getUserOrdersApi = (id: string) =>
